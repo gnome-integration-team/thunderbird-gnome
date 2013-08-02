5c0d9aa6f7993e5cace4434c4f8aee22b8b46a09
Alexander Seleznev <SeleznevRU@gmail.com>
2013-08-02 13:12:08 +0700

    Port bash scripts from firefox-gnome

--- make-xpi.sh
+++ make-xpi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 cd theme
-zip -r ../adwaita-thunderbird.xpi *
+
+VERSION=$(cat install.rdf | grep em:version | cut -f2 -d'>' | cut -f1 -d'<')
+
+zip -FS -r ../gnome-thunderbird-$VERSION.xpi *
+
 cd -
+
+ln -sf gnome-thunderbird-$VERSION.xpi gnome-thunderbird.xpi
